3a06ed2d140e26845d27a4f0a2887098cd825121
Pim Hakkert <p.t.hakkert@hotmail.com>
2019-06-04 21:08:59 +0200

    Added start of RESTful API route

--- web_api/index.php
+++ web_api/index.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Pim Hakkert
- * Date: 04/06/2019
- * Time: 19:30
- */
+//Temporary code
+    $user_1 = 'Jan';
+    $user_2 = 'Henk';
+    if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        $userRequest = $_GET['user'];
+        if($userRequest == 1) {
+            echo json_encode($user_1);
+        }
+        else {
+            echo json_encode($user_2);
+        }
+    }
+    elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    }
+?>
