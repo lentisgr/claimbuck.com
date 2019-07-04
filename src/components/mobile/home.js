212224bc2d5afc3db79d7b433f7a0f5be7129ba6
XRaiderGH <raigriffioen3@gmail.com>
2019-07-04 14:12:13 +0200

    fixed bug

--- src/components/mobile/home.js
+++ src/components/mobile/home.js
@@ -1,90 +1,83 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './css/home.css'
 
 import insightIcon from '../../images/insight icon.svg';
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
+const Home = () => {
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
         }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
+        return "";
     }
-    return "";
-}
 
-let cookieauthtoken = getCookie('cookieauthtoken');
-let cookieusername = getCookie('cookieusername');
-let accountInfo = 0;
+    let username = 'XRaider';
+    let cookieauthtoken = getCookie('cookieauthtoken');
+    let cookieusername = getCookie('cookieusername');
+    const [accountInfo, setAccountInfo] = useState([]);
 
-const getUserData = async () => fetch('https://claimbuck.com/web_api/index.php', {
-    method: 'POST',
-    headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-    },
-    body: 'request=userInfo&auth_token=' + 'vY6RiAxQoKHzdkZyEa2XLgIht7PcMGwlbu5qWN4Jp3jOVU1F' + '&username=' + 'ss'
-})
-    .then(function (response) {
-        return response.json();
+    useEffect(() => {
+        getUserData()
+    }, []);
+
+    const getUserData = async () => fetch('https://claimbuck.com/web_api/index.php', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: 'request=userInfo&auth_token=' + 'vY6RiAxQoKHzdkZyEa2XLgIht7PcMGwlbu5qWN4Jp3jOVU1F' + '&username=' + 'ss'
     })
-    .then(function (data) {
-        console.log(data);
-
-        accountInfo = data;
-
-        console.log(accountInfo);
-    });
-getUserData();
-
-console.log(accountInfo);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            setAccountInfo(data);
+        });
 
 
-
-
-
-
-
-
-class Home extends React.Component {
-    render() {
-        return (
-            <div className={'wrapper'}>
-                <div className="welcomeMessage">
-                    Hey, <br/> Welcome back!
-                </div>
-
-                <div className="insightsContainer">
-                    <div className="insightsHeader">
-                        <h1><span className={'icon'}><img src={insightIcon} alt=""/></span>Insights</h1>
-                    </div>
-                    <div className="insightsContent">
-                        <div className="insightsText">
-                            <ul>
-                                <li>Balance:</li>
-                                <li>Earnings:</li>
-                                <li>Offers completed:</li>
-                            </ul>
-                        </div>
-                        <div className="insightsData">
-                            <ul>
-                                <li className={'balance'}>{accountInfo.points}</li>
-                                <li>10</li>
-                                <li>10</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-
+    return (
+        <div className={'wrapper'}>
+            <div className="welcomeMessage">
+                Hey {username}, <br/> Welcome back!
             </div>
-        )
-    }
-}
+
+            <div className="insightsContainer">
+                <div className="insightsHeader">
+                    <h1><span className={'icon'}><img src={insightIcon} alt=""/></span>Insights</h1>
+                </div>
+                <div className="insightsContent">
+                    <div className="insightsText">
+                        <ul>
+                            <li>Balance:</li>
+                            <li>Earnings:</li>
+                            <li>Offers completed:</li>
+                        </ul>
+                    </div>
+                    <div className="insightsData">
+                        <ul>
+                            <li className={'balance'}>{accountInfo.points}</li>
+                            <li>{accountInfo.total_points}</li>
+                            <li>{accountInfo.completed_offers}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+};
+
 
 export default Home;
